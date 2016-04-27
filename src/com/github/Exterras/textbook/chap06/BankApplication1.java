f97b826bfe2392e6a2a57ee41dfa9bc951695f94
Exterras <choi3377@naver.com>
2016-04-28 00:28:57 +0900

    reading one

--- src/com/github/Exterras/textbook/chap06/BankApplication1.java
+++ src/com/github/Exterras/textbook/chap06/BankApplication1.java
@@ -5,20 +5,24 @@ import java.util.Scanner;
 public class BankApplication1 {
 	private static Account1[] accountArray = new Account1[100];
 	private static Scanner s = new Scanner(System.in);
-//	private static Account1 act = new Account1();
-	
-	private static int count = 0;
-	
+	private static int count = 0; // point
+
 	public static void main(String[] args) {
 		boolean run = true;
-		
-		while (run) {
 
-			for (int i = 0; i < 70; i++) {System.out.print("-");} System.out.println("");
-			System.out.println("1.Account Create" + "\t\t" + "2.Account List" + "\t\t" + "3.Deposit" + "\t" + "4.Withdraw"
-					+ "\t" + "5.Exit" + "\t");
-			for (int i = 0; i < 70; i++) {System.out.print("-");} System.out.println("");
+		while (run) {
+			for (int i = 0; i < 70; i++) {
+				System.out.print("-");
+			}
+			System.out.println("");
+			System.out.println("1.Account Create" + "\t\t" + "2.Account List" + "\t\t" + "3.Deposit" + "\t"
+					+ "4.Withdraw" + "\t" + "5.Exit" + "\t");
+			for (int i = 0; i < 70; i++) {
+				System.out.print("-");
+			}
+			System.out.println("");
 			System.out.print("Select> ");
+
 			int selectNo = s.nextInt();
 
 			if (selectNo == 1) {
@@ -38,62 +42,54 @@ public class BankApplication1 {
 		System.out.println("Program Terminated.");
 	}
 
-	private static void createAccount() {
-		String s1, s2;
-		int i1;
-		//implement
-		
-		//accountArray[count] = new Account1();
-		 
-		System.out.println("------------"+"\n"+"Account Create"+"\n"+"------------");
+	private static void createAccount() { // write
+		String ano, owner;
+		int balance; // point
+
+		System.out.println("------------" + "\n" + "Account Create" + "\n" + "------------");
 		System.out.print("Account Number : ");
-		s1 = s.next();
-		//accountArray[count].setAno(s.next());
+		ano = s.next();
+
 		System.out.print("Account Owner : ");
-		s2 = s.next();
-		//accountArray[count].setOwner(s.next());
+		owner = s.next();
+
 		System.out.print("First Deposit : ");
-		//accountArray[count].setBalance(s.nextInt());
-		i1 = s.nextInt();
+		balance = s.nextInt();
+
 		System.out.println("Account Created!");
-		
-		accountArray[count] = new Account1(s1 , s2, i1 );
-		
-		count++;
+
+		accountArray[count] = new Account1(ano, owner, balance); // main point
+		count++; // sub point -> next create & show list
 	}
 
-	private static void accountList() {
-		if(count == 0 ){
-			System.out.println("¿¡·¯");
+	private static void accountList() { // write
+		if (count == 0) {
+			System.out.println("Error");
 			return;
 		}
-		//implement
-//		System.out.println("----------"+"\n"+"Account List"+"\n"+"----------");
-//		System.out.println("Account"+"\t\t"+"Owner"+"\t"+"Deposit");
-//		System.out.print(accountArray[count].getAno() +"\t\t"+ accountArray[count].getOwner() +"\t"+ accountArray[count].getBalance());
-//		System.out.println("");
-		
-		System.out.println("----------"+"\n"+"Account List"+"\n"+"----------");
-		System.out.println("Account"+"\t\t"+"Owner"+"\t"+"Deposit");
-		for(int i=0; i < count; i++){						
-			System.out.print(accountArray[i].getAno() +"\t\t"+ accountArray[i].getOwner() +"\t"+ accountArray[i].getBalance());
+
+		System.out.println("----------" + "\n" + "Account List" + "\n" + "----------");
+		System.out.println("Account" + "\t\t" + "Owner" + "\t" + "Deposit");
+		for (int i = 0; i < count; i++) {
+			System.out.print(accountArray[i].getAno() + "\t\t" + accountArray[i].getOwner() + "\t"
+					+ accountArray[i].getBalance());
 			System.out.println("");
-		}
+		} // point
 	}
 
-	private static void deposit() {
-		//implement 
-		//call findAccount
-		System.out.println("------"+"\n"+"Deposit"+"\n"+"------");
+	private static void deposit() { // write
+		// call findAccount
+		System.out.println("------" + "\n" + "Deposit" + "\n" + "------");
 	}
 
-	private static void withdraw() {
-		//implement
-		//call findAccount
-		System.out.println("-------"+"\n"+"Withdraw"+"\n"+"-------");
+	private static void withdraw() { // write
+		// call findAccount
+		System.out.println("-------" + "\n" + "Withdraw" + "\n" + "-------");
 	}
-	
-/*	private static Account findAccount(String ano){
-		
-	}*/
+
+	/*
+	 * private static Account findAccount(String ano){
+	 * 
+	 * }
+	 */
 }
